36899237faef30f54ade441e243111887b2b6471
Yauheni Akhotnikau <eao197@users.noreply.github.com>
2019-01-28 18:56:43 +0300

    Simplification of CSP-based Dijkstra solution.

--- dev/dining_philosophers/csp_based/no_waiter_dijkstra/main.cpp
+++ dev/dining_philosophers/csp_based/no_waiter_dijkstra/main.cpp
@@ -53,9 +53,6 @@ void philosopher_process(
 {
 	int meals_eaten{ 0 };
 
-	// This flag is necessary for tracing of philosopher actions.
-	thinking_type_t thinking_type{ thinking_type_t::normal };
-
 	random_pause_generator_t pause_generator;
 
 	// This channel will be used for replies from forks.
@@ -63,13 +60,11 @@ void philosopher_process(
 
 	while( meals_eaten < meals_count )
 	{
-		tracer.thinking_started( philosopher_index, thinking_type );
+		tracer.thinking_started( philosopher_index, thinking_type_t::normal );
 
 		// Simulate thinking by suspending the thread.
-		std::this_thread::sleep_for( pause_generator.think_pause( thinking_type ) );
-
-		// For the case if we can't take forks.
-		thinking_type = thinking_type_t::hungry;
+		std::this_thread::sleep_for(
+				pause_generator.think_pause( thinking_type_t::normal ) );
 
 		// Try to get the left fork.
 		tracer.take_left_attempt( philosopher_index );
@@ -77,35 +72,32 @@ void philosopher_process(
 
 		// Request sent, wait for a reply.
 		so_5::receive( self_ch, so_5::infinite_wait,
-				[&]( so_5::mhood_t<taken_t> ) {
-					// Left fork is taken.
-					// Try to get the right fork.
-					tracer.take_right_attempt( philosopher_index );
-					so_5::send< take_t >(
-							right_fork, self_ch->as_mbox(), philosopher_index );
+			[&]( so_5::mhood_t<taken_t> ) {
+				// Left fork is taken.
+				// Try to get the right fork.
+				tracer.take_right_attempt( philosopher_index );
+				so_5::send< take_t >(
+						right_fork, self_ch->as_mbox(), philosopher_index );
 
-					// Request sent, wait for a reply.
-					so_5::receive( self_ch, so_5::infinite_wait,
-							[&]( so_5::mhood_t<taken_t> ) {
-								// Both fork are taken. We can eat.
-								tracer.eating_started( philosopher_index );
+				// Request sent, wait for a reply.
+				so_5::receive( self_ch, so_5::infinite_wait,
+					[&]( so_5::mhood_t<taken_t> ) {
+						// Both fork are taken. We can eat.
+						tracer.eating_started( philosopher_index );
 
-								// Simulate eating by suspending the thread.
-								std::this_thread::sleep_for( pause_generator.eat_pause() );
+						// Simulate eating by suspending the thread.
+						std::this_thread::sleep_for( pause_generator.eat_pause() );
 
-								// One step closer to the end.
-								++meals_eaten;
+						// One step closer to the end.
+						++meals_eaten;
 
-								// Right fork should be returned after eating.
-								so_5::send< put_t >( right_fork );
+						// Right fork should be returned after eating.
+						so_5::send< put_t >( right_fork );
+					} );
 
-								// Next thinking will be normal, not 'hungry_thinking'.
-								thinking_type = thinking_type_t::normal;
-							} );
-
-					// Left fork should be returned too.
-					so_5::send< put_t >( left_fork );
-				} );
+				// Left fork should be returned too.
+				so_5::send< put_t >( left_fork );
+			} );
 
 	}
 
